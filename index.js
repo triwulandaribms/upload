5a60e1c98a7195d34c2456e94719ecc30d8fc389
Tri Wulandari <triwulandari3456@gmail.com>
2023-01-14 16:28:46 +0700

    tambah judul

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from "express";
 import { readFile } from "node:fs/promises";
-// import { writeFile } from "node:fs/promises";
-// import { appendFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
+import { appendFile } from "node:fs/promises";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/api/lihat", async (req, res) => {
+app.get("/api/lihat", async (_req, res) => {
   const data = await readFile("./skripsi.txt", "UTF8");
   res.send(data);
+});
+
+app.post("/api/tulis", async (req, res) => {
+  await writeFile("./skripsi.txt", req.body.data, "UTF8");
+  res.send(`berhasil menulis file`);
 });
 
 app.listen(3000, () => console.log("berhasil dijalankan"));
